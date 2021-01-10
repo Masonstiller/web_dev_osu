5037dcd242c9b24947f0c3c7610802a7cb640db6
Masonstiller <67441648+Masonstiller@users.noreply.github.com>
2021-01-10 15:16:06 -0800

    Add files via upload

---
+++ temp_convert.py
@@ -0,0 +1,13 @@
+# Author: Mason Stiller
+# Date: 6/30/20
+# Description: Asks the user for a temperature
+# in Celsius and converts it to Fahrenheit
+
+print("Please enter a Celsius temperature.")
+
+C = float(input())
+
+F = ((9/5) * C) + 32
+
+print("The equivalent Fahrenheit temperature is:")
+print(F)
